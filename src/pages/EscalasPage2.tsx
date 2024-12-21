aa782045d924f45fb82ac555b7a211bc4df7c29c
Rodrigo Gomes <rodrigomes002@gmail.com>
2024-12-21 01:42:47 -0300

    - Ajustando configuração do pwa

--- src/pages/EscalasPage2.tsx
+++ src/pages/EscalasPage2.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Plus, Edit, Trash, Music, Users, Loader2 } from "lucide-react";
 import {
   Card,
